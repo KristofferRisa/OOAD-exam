9c5322e0e8ac09f3e8bacb27397d623dcd5ca752
Kristoffer Risa <kristofferrisa@gmail.com>
2017-11-25 19:47:26 +0100

    Lagt til tekst på objekt diagram og endre på CSS

--- innlevering/js/script.js
+++ innlevering/js/script.js
@@ -1,18 +1,3 @@
-$(document).ready(function () { 
-    //Kjøres når alt HTML innhold er ferdig lastet ned
-    $(window).scroll(function () {  
-        //Nav-bar sticky script ved scrolling
-        //Legger til classen "navbar-fixed" på elementet med id=nav når man har scrollet 143 pixels ned slik at navbar legges fast på toppen av siden           
-        if ($(window).scrollTop() > 2) {
-            $('#nav').addClass('navbar-fixed');
-        } 
-        if ($(window).scrollTop() < 2) {
-            $('#nav').removeClass('navbar-fixed');        
-        }
-    });
-});
-
-
 function showStruktur()  {
     document.getElementById("diagram").classList.add('hide');
     document.getElementById("atferds").classList.add('hide');
