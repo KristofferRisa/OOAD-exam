4c2ee1f29071e5964a3de7b23b757e462b69c462
Kristoffer Risa <kristofferrisa@gmail.com>
2017-11-26 15:03:25 +0100

    lage JS som generer UML tre og oppdatert klassediagram og objektdiagram

--- innlevering/js/script.js
+++ innlevering/js/script.js
@@ -27,8 +27,9 @@ function visOversikt() {
 }
 
 (function () {
-    _location = getParameterByName('location'); // "lorem"
-    console.log("foo == " + _location);
+    createUmlTree();
+    _location = getParameterByName('location'); 
+    console.log("?location= " + _location);
     switch (_location) {
         case "s":
             showStruktur();
@@ -40,8 +41,16 @@ function visOversikt() {
             showInteraksjon();
             break;
     }
+   _active = getParameterByName('active');
+    console.log("?active="+_active)
+    setActiveById(_active);
+
 })();
 
+
+function setActiveById(_id){
+    document.getElementById(_id).classList.add('active');
+}
 
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -51,4 +60,128 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function createUmlTree(){
+    var html = `
+<div class="tree" id="diagram">
+    <ul>
+        <li>
+            <span>Diagram</span>
+            <ul>
+                <li>
+                    <a href="#struktur?location=s" onclick="showStruktur();">Struktur</a>
+                </li>
+
+                <li>
+                    <a href="#atferds?location=a" onclick="showAtferds();">Atferds</a>
+
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<div class="tree hide" id="struktur">
+    <ul>
+        <li>
+            <a href="#" onclick="visOversikt();">Diagram</a>
+            <ul>
+                <li>
+                    <span>Struktur</span>
+                    <ul>
+                        <li>
+                            <a id="klasse" href="klassediagram.html?location=s&active=klasse">Klasse</a>
+                        </li>
+                        <li>
+                            <a id="objekt" href="objektdiagram.html?location=s&active=objekt">Objekt</a>
+                        </li>
+                        <li>
+                            <a id="pakke" href="pakkediagram.html">Pakke</a>
+                        </li>
+                        <li>
+                            <a id="komponent" href="#">Komponent</a>
+                        </li>
+                        <li>
+                            <a id="utplassering" href="#">Utplasserings</a>
+                        </li>
+                        <li>
+                            <a id="sammesatt" href="#">Sammensatt struktur</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+    </ul>
+    </li>
+
+</div>
+
+<div class="tree hide" id="atferds">
+    <ul>
+        <li>
+            <a href="#" onclick="visOversikt();">Diagram</a>
+            <ul>
+                <li>
+                    <span>Atferds</span>
+                    <ul>
+                        <li>
+                            <a onclick="showInteraksjon();" href="#interaksjon?location=i">Interaksjons</a>
+                        </li>
+                        <li>
+                            <a id="usecase" href="#">Bruksmønster</a>
+                        </li>
+                        <li>
+                            <a id="aktivitetsdiagram" href="aktivitetsdiagram">Aktivitets</a>
+                        </li>
+                        <li>
+                            <a id="tilstand" href="tilstandsdiagram.html">Tilstand</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+</div>
+
+<div class="tree hide diagram-wide" id="interaksjon">
+    <ul>
+        <li>
+            <a href="#" onclick="visOversikt();">Diagram</a>
+            <ul>
+                <li>
+                    <a href="#" onclick="showAtferds();">Atferds</a>
+
+                    <ul>
+                        <li>
+                            <span>Interaksjons</span>
+                            <ul>
+                                <li>
+                                    <a id="sekvensdiagram" href="Sekvensdiagram.html">Sekvens</a>
+                                </li>
+                                <li>
+                                    <a id="kommunikasjon" href="kommunikasjonsdiagram.html">Kommunikasjons</a>
+                                </li>
+                                <li>
+                                    <a id="tidsdeling" href="tidsdelingsdiagram.html">Tidsdelings</a>
+                                </li>
+                                <li>
+                                    <a id="interaksjon" href="#">Interaksjonsoversikt</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+    </ul>
+    </li>
+</div>
+`;
+
+    var tree = document.getElementById('tree');
+    if(tree){
+        tree.innerHTML = html;
+    }
 }
