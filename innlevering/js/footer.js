e4b4109b55223d7bae6b4439728249caeffdb73a
Kristoffer Risa <kristofferrisa@gmail.com>
2017-11-26 21:52:56 +0100

    Rydde opp

--- innlevering/js/footer.js
+++
@@ -1,6 +0,0 @@
-document.write('<footer><p>|&nbsp;&nbsp;<a href="http://www.hbv.no/studietilbud/bachelor-i-informasjonssystemer-og-it-ledelse/">Skoleprosjekt - Informasjonssystemer og IT-ledelse på Buskerud og Vestfold Høyskolen</a>&nbsp;&nbsp; |&nbsp;&nbsp;');
-document.write('<a href="https://www.hbv.no/">Besøk skolens hjemmeside</a> &nbsp;&nbsp;|&nbsp;&nbsp;');
-document.write('<a href="http://wedontknow.codes">Mer info om gruppen finnes her</a>&nbsp;&nbsp;|</p>');
-document.write('<p>|&nbsp;&nbsp;');
-document.write('<a href="mailto:post@wedontknow.codes">Sends oss en mail</a>&nbsp;&nbsp;|</p>');
-document.write('</footer>');
