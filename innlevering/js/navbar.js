6a832ee5c9d5608f3ef624e9ffa2df23f5d9a65e
Kristoffer Risa <kristofferrisa@gmail.com>
2017-11-26 23:14:15 +0100

    oppdatert navbar

--- innlevering/js/navbar.js
+++ innlevering/js/navbar.js
@@ -1,5 +1,5 @@
 document.write('<nav class="navheader" id="nav" class="nav"><ul><li><a href="../index.html">Hovedsiden</a></li>');
-document.write('<li><a href="../html/oppgave.html">Vår løsning</a></li>');
+document.write('<li><a href="../html/oppgave.html">Vår besvarelse</a></li>');
 document.write('<li><a href="../html/diagrammer.html">Diagrammer</a></li>')
 document.write('<li><a href="../MEDLEMMER.html">Medlemmer</a></li>');
 document.write('<li><a href="../html/litteraturliste.html">Litteraturliste</a></li></ul></nav>');
